5952755308e8fbf93191dc4d2ac9f77e5b6b8f8e
vaughan189 <vaughan188@gmail.com>
2020-05-05 15:39:51 +0530

    unit test issue fixed

--- tests/Greeter.test.ts
+++ tests/Greeter.test.ts
@@ -1,5 +1,5 @@
 import { test, expect } from '@jest/globals';
-import Greeter from '../src/index';
+import { Greeter } from '../src/index';
 
 test('My Greeter', () => {
   expect(Greeter('Carl')).toBe('Hello Carl');
